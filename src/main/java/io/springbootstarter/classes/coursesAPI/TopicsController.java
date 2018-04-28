bfcdaf56e4114743a294f832db847469ed0927a7
chirag.p <chirag.p@loginextsolutions.com>
2018-04-29 00:36:01 +0530

    Adding data

--- src/main/java/io/springbootstarter/classes/coursesAPI/TopicsController.java
+++ src/main/java/io/springbootstarter/classes/coursesAPI/TopicsController.java
@@ -12,11 +12,12 @@ public class TopicsController {
 	@RequestMapping ("/topics")
 	public List <Topics> getTopics (){
 		List <Topics> topicsList = new ArrayList<>();
-		return topicsList;
-		/*	
+
 		topicsList.add(new Topics("1", "Test Automation", "Fedric Beta"));		
 		topicsList.add(new Topics("2", "Penetration Testing", "Fedric Beta"));		
 		topicsList.add(new Topics("3", "jQuery Testing", "Joe San"));		
-		return topicsList;*/
+		return topicsList;
+		return topicsList;
+		
 	}
 }
