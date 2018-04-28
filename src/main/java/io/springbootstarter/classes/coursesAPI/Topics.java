63ab411b56568e619d9f053b8a6a00b0aefb090a
chirag.p <chirag.p@loginextsolutions.com>
2018-04-29 00:05:53 +0530

    Added topics class and returning List

--- src/main/java/io/springbootstarter/classes/coursesAPI/Topics.java
+++ src/main/java/io/springbootstarter/classes/coursesAPI/Topics.java
@@ -1,5 +1,34 @@
 package io.springbootstarter.classes.coursesAPI;
 
 public class Topics {
-
+	String id;
+	String strTopicName;
+	String author;
+	public Topics() {
+		
+	}
+	public Topics(String id, String strTopicName, String author) {
+		super();
+		this.id = id;
+		this.strTopicName = strTopicName;
+		this.author = author;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getStrTopicName() {
+		return strTopicName;
+	}
+	public void setStrTopicName(String strTopicName) {
+		this.strTopicName = strTopicName;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
 }
--- src/main/java/io/springbootstarter/classes/coursesAPI/TopicsController.java
+++ src/main/java/io/springbootstarter/classes/coursesAPI/TopicsController.java
@@ -1,5 +1,9 @@
 package io.springbootstarter.classes.coursesAPI;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.assertj.core.util.Arrays;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class TopicsController {
 
 	@RequestMapping ("/topics")
-	public String getTopics (){
-		return "Hi";
+	public List <Topics> getTopics (){
+		List <Topics> topicsList = new ArrayList<>();
+		topicsList.add(new Topics("1", "Test Automation", "Fedric Beta"));		
+		topicsList.add(new Topics("2", "Penetration Testing", "Fedric Beta"));		
+		topicsList.add(new Topics("3", "jQuery Testing", "Joe San"));		
+		return topicsList;
 	}
 }
